1dd594215c4525754a3a7cb3382834931209fcf8
Pack Fan 1031 <ocskier@gmail.com>
2018-08-26 17:48:46 -0400

    latest revision

--- bamazon.sql
+++ bamazon.sql
@@ -13,8 +13,10 @@ CREATE TABLE items (
 );
 
 INSERT INTO items (name, descr, price,quantity)
-VALUES ("stereo", "Panasonic 200W",450.00, 6),("speakers", 'Panasonic 6.5" Two-Way 100W ',65.00, 12),
-("tv", 'Sony 52" 1080P HDTV',795.00, 5),("ipod", "80GB Ipod Video 6th Gen",100.00, 2),
-("iphone", "Iphone 6S Plus 64GB",545.00, 10),("hard drive", "Samsung 1TB SSD",219.95, 8);
+VALUES ("stereo", "Panasonic 200W",450.99, 6),("speakers", 'Panasonic 6.5" Two-Way 100W ',65.59, 12),
+("tv", 'Sony 52" 1080P HDTV',795.99, 5),("ipod", "80GB Ipod Video 6th Gen",100.99, 2),
+("iphone", "Iphone 6S Plus 64GB",545.89, 10),("hard drive", "Samsung 1TB SSD",219.95, 8),
+("video card", 'Nvidia GeForce 1080Ti',679.99, 8),("sound card", "SoundBlaster Audigy 2",97.99,11),
+("soundbar", 'Sony 100W All-in-One Surround',195.99, 6),("game console", "XBox One S 1 TB",321.99, 18);
 
 
---
+++ customer.js
@@ -0,0 +1,72 @@
+var inq = require("inquirer");
+var mysql = require("mysql");
+
+const cTable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "bamazon_DB"
+});
+  
+
+function getJob() {
+  inq.prompt([
+    {
+        name: "job",
+        message: "What is your role at Bamazon?",
+        type: "input"
+    },
+    {
+        name: "confirm",
+        message: "Are you sure?",
+        type: "confirm",
+        default:true
+    }
+  ]).then((value) => {
+    if (value.confirm) {
+        switch(value.job.toLowerCase()) {
+            case "employee": 
+                console.log("You are an employee!!");
+                break;
+            case "manager":
+                console.log("You are the manager!!");
+                break;
+            case "customer":
+                console.log("Hey how are you doing today?");
+                console.log("Here is a list of items for sale:");
+                showCustProds();
+                break;
+            default:
+                getJob();
+        }
+    }
+  });
+}
+
+function showCustProds() {
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log("connected as id " + connection.threadId + "\n");
+        connection.query("select * from items", function(err,res) {
+            if(err) throw err;
+            var itemArray = [];
+            for (let i = 0; i < res.length; i++) {
+                itemArray.push(res[i]);
+            }
+            const table = cTable.getTable(itemArray);
+            console.log(table);
+            connection.end();
+        });
+    });
+}
+
+getJob();
